1526c1aac2540558ac880ec380edb1c860d5e465
Andrew Plaza <andrew.plaza@parity.io>
2021-05-19 21:35:36 -0400

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+    do_a_backflip();
 }
+
+fn do_a_backflip() {
+    println!("Just some changes to create a release with");
+    let mut counter = 0;
+
+    loop { 
+        counter += 1;
+        if counter == 1_000_000 {
+            break
+        }
+    }
+
+    println!("Goodbye");
+}
+
+
